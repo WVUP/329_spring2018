eb03ba7401584639a9a6e99571a4beecf56c58b4
Levi Butcher <lbutche3@wvup.edu>
2018-02-20 19:54:48 -0500

    gotta go fast

--- assignments/pokedex/Levi-gen2-best/src/index.js
+++ assignments/pokedex/Levi-gen2-best/src/index.js
@@ -1,6 +1,7 @@
 import {render} from 'react-dom';
 import React, {Component} from 'react';
 import fetch from 'isomorphic-fetch';
+import { isNull } from 'util';
 
 
 //The Pokemon component will show an individual Pokemon monster
@@ -45,14 +46,42 @@ class PokemonList extends Component{
       this.setState({
         species : response.results,
         loading : true,
-        fetched : true
+        fetched : true,
       });
     });
   }
 
+  filterOutPokemon(find)
+  {
+    let pokemans = this.state.species;
+    console.log(pokemans);
+    let filterMans = pokemans.filter(pokemans =>{
+      console.log(pokemans.pokemon.name);
+      return pokemans.pokemon.name.contains(find);
+    });
+
+    this.setState({
+      species: filterMans
+    })
+  }
+
   render(){
     const {fetched, loading, species} = this.state;
-    let content ;
+    let content;
+    let inputEle = document.getElementById(42);
+
+    if(inputEle != null)
+    {
+      let target = inputEle.target;
+      //console.log(value);
+      console.log("Hello world");
+      if(target != null)
+      {
+        console.log("Hello world");
+        filterOutPokemon(target.value);
+      }
+    }
+    
     if(fetched){
       content = <div className="pokemon--species--list">{species.map((pokemon,index)=><Pokemon key={pokemon.name} id={index+1} pokemon={pokemon}/>)}</div>;
     }else if(loading && !fetched){
@@ -62,11 +91,47 @@ class PokemonList extends Component{
       content = <div/>;
     }
     return  <div>
-      {content}
+      <div>
+        <FindPokemon/>
+      </div>
+      <div>
+         {content}
+      </div>
     </div>;
   }
 }
 
+// Used for Form Input => https://reactjs.org/docs/forms.html
+class FindPokemon extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      find: ""
+    }
+
+    this.captureInput = this.captureInput.bind(this);
+  }
+
+  captureInput(input){
+    console.log(input.target.value);
+    this.setState({
+      find: input.target.value,
+    });
+  }
+  
+  render(){
+
+    let find = this.state.find;
+    let finder = <input type="text" id={42} value={this.state.find} onChange={this.captureInput}/>
+    let form = (<div>
+      <form onSubmit={(e) => e.preventDefault()}> 
+        {finder}
+      </form>
+    </div>);
+
+    return form;
+  }
+}
 
 //This is the root component
 class PokeApp extends Component{
