a410fb69eff25dbe246115cfc231aa31c551fc91
Aaron Freeland <afreeland@gmail.com>
2018-02-26 17:54:25 -0500

    Starter project for everyone...

---
+++ twitch_starter/src/scripts/Banner.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Banner(props) {
+    return (
+        <div className="page-header">
+            <h1>Twitch App!</h1>
+        </div>
+    );
+};
