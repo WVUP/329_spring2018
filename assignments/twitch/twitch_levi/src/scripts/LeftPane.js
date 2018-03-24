16a45a40a9cee08c5f3c473ccf91a815e7df726a
Levi Butcher <lbutche3@wvup.edu>
2018-03-24 13:31:36 -0400

    getting there

--- assignments/twitch/twitch_levi/src/scripts/LeftPane.js
+++ assignments/twitch/twitch_levi/src/scripts/LeftPane.js
@@ -6,6 +6,8 @@ export default function LeftPane(props){
         <div className="container">
             <div className="leftpane">
                 <SiteImagesBox/>
+                <UserBox/>
+                <UserContent/>
             </div>
         </div>
     );
@@ -13,8 +15,65 @@ export default function LeftPane(props){
 
 const SiteImagesBox = (props)=>{
     return (
-        <div id="twitchLogo">
-            <img src="./src/images/twitch.png" alt="Twitch Logo"/>
+        <div id="twitchLogo" >
+            <img className="img-responsive" src="./src/images/twitchName.png" alt="Twitch Logo"/>
+        </div>
+    );
+}
+
+
+const UserBox = (props)=>{
+    return(
+        <div id="userBox" className="media">
+            <div className="media-left">
+                <img id="userPic" className="media-object" src="./src/images/userPic.jpg" alt=""/>
+            </div>
+            <div id="userName" className="media-body">
+                <h2 className="media-heading">User Name</h2>
+                <p><span className="glyphicon glyphicon-stop" style={{color:'lightgreen'}}></span> Online</p>
+            </div>
+        </div>
+    );
+}
+
+
+
+const FollowedChannelBox = (props) =>{
+    return(
+        <div className="textAlign">
+            <a href="#userFollowers" data-toggle='collapse'><span className="glyphicon glyphicon-chevron-down"></span></a>
+            <div id="userFollowers" className="collapse">
+                <div className="media">
+                    <div className="media-left">
+                        <img className="media-object" src="./src/images/userPic.jpg" alt="Streamer Picture"/>
+                    </div>
+                    <div id="userName" className="media-body">
+                        <h2 className="media-heading">User Name</h2>
+                        <p><span className="glyphicon glyphicon-stop" style={{color:'lightgreen'}}></span> Online</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+const SearchBox = (props)=>{
+    return(
+         <div id="searchBox">
+            <div className="form-inline">
+                <label></label>
+                <input className="form-control" type="search"></input>
+                <button className="btn btn-primary"><span className="glyphicon glyphicon-search" style={{color:'white'}}></span></button>
+            </div>
+        </div>
+    );
+}
+
+const UserContent = (props) =>{
+    return (
+         <div id="userContent">
+            <FollowedChannelBox/>
+            <SearchBox/>
         </div>
     );
 }
---
+++ assignments/twitch/twitch_levi/src/scripts/RightPane.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function RightPane(props){
+    return (
+        <div className="container">
+            <div className="rightpane">
+                <span id="arrowone" className="glyphicon glyphicon-chevron-right"></span>
+                <span id="arrowtwo" className="glyphicon glyphicon-chevron-right"></span>
+                <span id="arrowthree" className="glyphicon glyphicon-chevron-right"></span>
+            </div>
+        </div>
+    );
+}
