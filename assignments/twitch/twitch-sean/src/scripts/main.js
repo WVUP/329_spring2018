97987bac38c39dc40a0e007ced6f2e493e954b07
seanrickard <srickard@wvup.edu>
2018-03-26 17:27:12 -0400

    started

---
+++ assignments/twitch/twitch-sean/src/scripts/main.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import TwitchApp from './TwitchApp';
+
+import '../styles/styles.scss';
+import 'bootstrap-sass/assets/stylesheets/_bootstrap.scss';
+
+
+ReactDOM.render(<TwitchApp/>, document.getElementById('react-container'));
