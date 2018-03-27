b9f06728b8fe0e14134377e2e658fcd395e6c90b
Darienne Miller <31522424+darienne@users.noreply.github.com>
2018-03-27 13:58:42 -0400

    twitch

---
+++ assignments/twitch/Twitch-Dar/src/scripts/TwitchApp.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Banner from './Banner';
+
+export default function TwitchApp(props){
+    return (
+        <div>
+            <Banner />
+            <div className="container">
+                <p>Starter twitch app</p>
+            </div>
+        </div>
+    );
+};
