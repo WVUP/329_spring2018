1f2a10bb31979595c934b3a0de757237b70d9778
Levi Butcher <lbutche3@wvup.edu>
2018-04-01 23:39:56 -0400

    Added in Middle Pane

--- assignments/twitch/twitch_levi/src/scripts/TwitchApp.js
+++ assignments/twitch/twitch_levi/src/scripts/TwitchApp.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Banner from './Banner';
 import LeftPane from './LeftPane';
 import RightPane from './RightPane';
+import MiddlePane from './MiddlePane';
 
 export default function TwitchApp(props){
     return (
         <div className="container-fluid">
             <LeftPane/>
-            <RightPane/>
+            <MiddlePane />
+            {/* <RightPane/> */}
         </div>
     );
 };
