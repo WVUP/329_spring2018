128a2eaab3f29737bae7b0c99993531d73e87d22
EthanRhodes <erhodes7@wvup.edu>
2018-04-23 13:15:00 -0400

    minor changes
    minor changes

---
+++ assignments/twitch/EthanRhodes_Twitch/src/scripts/Center.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Component} from 'react';
+import axios from 'axios';
+axios.defaults.headers.common['Client-Id'] = "5hclx4xrr4qpx18lvaoc9gsxh4xkcg";
+
+export default class LeftSide extends Component
+{
+    constructor(props)
+    {
+        super(props);
+        this.state={};
+    }
+}
