178f1a44d291e9e950532f3ef64281ec899765b9
Levi Butcher <lbutche3@wvup.edu>
2018-04-25 23:50:13 -0400

    deleted old project

--- assignments/twitch/twitch_levi/src/scripts/Banner.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Banner(props) {
-    return (
-        <div className="page-header">
-            <h1>Twitch App!</h1>
-        </div>
-    );
-};
